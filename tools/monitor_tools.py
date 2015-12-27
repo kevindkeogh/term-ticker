f79de71487f0bbe64642fb8cc10ddf0033f2030c
Kevin Keogh <kevinkeogh@Kevins-MacBook-Air-2.local>
2015-12-27 17:37:02 -0500

    fix adding tables

--- tools/monitor_tools.py
+++ tools/monitor_tools.py
@@ -12,12 +12,16 @@ def setup_monitor_tables(cursor):
     cursor.execute(sql_create_table_monitor_feeds)
     cursor.execute(sql_create_table_monitor_prices)
     add_monitor_prices_columns(cursor)
-    
-
 
 def add_sql_column(cursor, table_name, column_name):
     command = '''ALTER TABLE {0} ADD COLUMN '{1}' REAL'''.format(table_name, column_name)
     cursor.execute(command)
+
+def add_index_monitor(cursor, display_name, ticker_code):
+    command = '''INSERT OR IGNORE INTO MONITOR_FEEDS
+                 VALUES ('{0}', '{1}')'''.format(display_name, ticker_code)
+    cursor.execute(command)
+    return
 
 def add_monitor_prices_columns(cursor):
     tickers_columns = cursor.execute(   '''SELECT ticker_code
@@ -28,10 +32,7 @@ def add_monitor_prices_columns(cursor):
     prices_columns.remove('date')
     # If empty, add SPX
     if prices_columns == []:
-        cursor.execute('''INSERT INTO MONITOR_FEEDS
-                          VALUES ('SPX', '^GSPC')
-                       '''
-                       )
+        add_index_monitor(cursor, 'SPX', '^GSPC')
         prices_columns  = [info[1] for info in cursor.execute('PRAGMA table_info(MONITOR_PRICES)')]
         tickers_columns = cursor.execute(   '''SELECT ticker_code
                                             FROM MONITOR_FEEDS
@@ -41,7 +42,3 @@ def add_monitor_prices_columns(cursor):
     for ticker in tickers_columns:
         if ticker not in prices_columns:
             add_sql_column(cursor, 'MONITOR_PRICES', ticker[0])
-
-connection = sqlite3.connect('sample.db')
-cursor = connection.cursor()
-setup_monitor_tables(cursor)
